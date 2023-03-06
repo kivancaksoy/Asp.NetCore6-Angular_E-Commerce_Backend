f79f8985de8b5fe47cfc2405bf2b3aeaa0067df6
kivancaksoy <kvnc96@hotmail.com>
2023-03-06 13:33:27 +0300

    GoogleLogin added.

---
+++ Core/ECommerceBE.Application/Features/Commands/AppUser/GoogleLogin/GoogleLoginCommandResponse.cs
@@ -0,0 +1,14 @@
+﻿using ECommerceBE.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceBE.Application.Features.Commands.AppUser.GoogleLogin
+{
+    public class GoogleLoginCommandResponse
+    {
+        public Token Token { get; set; }
+    }
+}
